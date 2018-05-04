dc8179d3278e46ab12287f9f7f2cee53f5228bae
Kevin Barnes <kbarnes3@gmail.com>
2018-05-03 23:01:38 -0700

    Adding real conclaves

--- mystic-vale/src/app/conclave-data.ts
+++ mystic-vale/src/app/conclave-data.ts
@@ -3,19 +3,21 @@ import { Conclave } from './conclave';
 export const NUMBER_OF_REQUIRED_CONCLAVES = 2;
 
 export const REQUIRED_CONCLAVES: Conclave[] = [
-  { name: 'Basic 1' },
-  { name: 'Basic 2' },
-  { name: 'Basic 3' }
+  { name: 'Early Harvest' },
+  { name: 'Eternal Dawn' },
+  { name: 'Homeland' },
+  { name: 'Mythic Beasts' },
+  { name: 'Nature\'s Vitality' },
+  { name: 'Nesting Peaks' },
+  { name: 'Open the Gateway' },
+  { name: 'Staying Power' },
+  { name: 'Taming the Wild' }
 ];
 
 export const EXTRA_CONCLAVES: Conclave[] = [
-  { name: 'Fancy 1' },
-  { name: 'Fancy 2' },
-  { name: 'Fancy 3' },
-  { name: 'Fancy 4' },
-  { name: 'Fancy 5' },
-  { name: 'Fancy 6' },
-  { name: 'Fancy 7' },
-  { name: 'Fancy 8' },
-  { name: 'Fancy 9' }
+  { name: 'Riding the Brink' },
+  { name: 'Secret Sanctuaries' },
+  { name: 'Seeds of Bounty' },
+  { name: 'Seekers of the Sun' },
+  { name: 'Upwelling' }
 ];
